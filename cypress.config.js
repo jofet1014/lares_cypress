83b9b764983dc402a665499c9ae0c8251bddfd6e
jofet1014 <85337116+jofet1014@users.noreply.github.com>
2023-01-31 11:19:39 +0800

    main

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://lares-frontend-qa.apps.bcstechnology.com.au/',
-    chromeWebSecurity: false,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
